bb91d69180d1b2c6612d065c56ba90298c30546a
Anderson Melo <142186594+AndersonMeloo@users.noreply.github.com>
2026-02-28 10:51:11 -0300

    feat: adiciona componente Categorias na Home > Integra categorias com imagens e textos na tela inicial

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,5 @@
 import Header from "../components/Header/Header";
+import Categoria from "../components/Categorias/Categorias";
 
 function Home() {
 
@@ -6,6 +7,17 @@ function Home() {
 
         <>
             <Header />
+
+            <div className="containerHomeCategorias">
+                <Categoria imageSrc="/imagensCategorias/Tecnologia.png" text="Tecnologia" />
+                <Categoria imageSrc="/imagensCategorias/SuperMercado.png" text="Supermercado" />
+                <Categoria imageSrc="/imagensCategorias/whiskey.png" text="Bebidas" />
+                <Categoria imageSrc="/imagensCategorias/ferramentas.png" text="Ferramentas" />
+                <Categoria imageSrc="/imagensCategorias/cuidados-de-saude.png" text="Saúde" />
+                <Categoria imageSrc="/imagensCategorias/corrida.png" text="Esportes e Fitness" />
+                <Categoria imageSrc="/imagensCategorias/moda.png" text="Moda" />
+            </div>
+
         </>
     );
 }
