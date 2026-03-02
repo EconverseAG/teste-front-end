e8dece15a1915536dfd7c25211e627604aa5de5c
sZmanu <manuellaalvesdeoliveira62@gmail.com>
2026-03-02 20:06:16 -0300

    Projeto finalizado

--- test-front-end/src/App.tsx
+++ test-front-end/src/App.tsx
@@ -2,6 +2,12 @@ import NavBar from './components/nav/NavBar'
 import './index.scss'
 import Banner from './components/banner/Banner'
 import CategoriaLista from './components/categoria/CategoriaLista'
+import ListaMarcas from './components/marcas/ListaMarcas'
+import Footer from './components/footer/Footer'
+import CategoriaProduto from './components/produto/titulo/CategoriaProduto'
+import TituloSelecionado from './components/produto/titulo/Selecionado'
+import ListaParceiros from './components/parceiros/ListaParceiros'
+import ListaProduto from './components/produto/ListaProduto'
 
 function App() {
 
@@ -10,6 +16,16 @@ function App() {
     <NavBar />
     <Banner />
     <CategoriaLista/>
+    <CategoriaProduto />
+    <ListaProduto/>
+    <ListaParceiros/>
+    <TituloSelecionado/>
+    <ListaProduto/>
+    <ListaParceiros/>
+    <ListaMarcas/>
+    <TituloSelecionado/>
+    <ListaProduto/>
+    <Footer/>
         
     </>
   )
